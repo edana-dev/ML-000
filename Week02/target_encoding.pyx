b60d373c198ba89b0087c2e5bc6014edf7daa007
Season <zh_season@163.com>
2021-01-12 23:49:33 +0800

    extract name.

--- Week02/target_encoding.pyx
+++ Week02/target_encoding.pyx
@@ -16,12 +16,13 @@ cpdef target_mean_v2(data, y_name, x_name):
     value_dict = dict()
     count_dict = dict()
     for i in range(data.shape[0]):
-        if data.loc[i, x_name] not in value_dict.keys():
-            value_dict[data.loc[i, x_name]] = data.loc[i, y_name]
-            count_dict[data.loc[i, x_name]] = 1
+        name = data.loc[i, x_name]
+        if name not in value_dict.keys():
+            value_dict[name] = data.loc[i, y_name]
+            count_dict[name] = 1
         else:
-            value_dict[data.loc[i, x_name]] += data.loc[i, y_name]
-            count_dict[data.loc[i, x_name]] += 1
+            value_dict[name] += data.loc[i, y_name]
+            count_dict[name] += 1
     for i in range(data.shape[0]):
         result[i] = (value_dict[data.loc[i, x_name]] - data.loc[i, y_name]) / (count_dict[data.loc[i, x_name]] - 1)
     return result
